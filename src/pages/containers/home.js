5fd6fc0c1251cc86befae9ea64bd20b51723197a
RaphaLeon <rapha.leon@gmail.com>
2018-07-21 01:50:48 -0400

    add VideoPlayer component

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -4,8 +4,8 @@ import Categories from '../../categories/components/categories';
 import Related from '../components/related';
 import ModalContainer from '../../widgets/containers/modal';
 import Modal from '../../widgets/components/modal';
-import handleError from '../../error/containers/handle-error';
 import HandleError from '../../error/containers/handle-error';
+import VideoPlayer from '../../player/containers/video-player';
 
 class Home extends Component {
     state = {
@@ -28,6 +28,7 @@ class Home extends Component {
             <HandleError>
                 <HomeLayout>    
                     <Related />
+                    <VideoPlayer />
                     <Categories 
                         categories={ this.props.data.categories }
                         handleOpenModal = { this.handleOpenModal }
